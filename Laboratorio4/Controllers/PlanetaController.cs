baf787e533f3c41a89422005e6a7c60c51074362
Jonathan_Navarro <jonathan.navarro00@outlook.es>
2021-10-01 14:25:45 -0600

    Crear formulario para que el colaborador pueda modificar sus datos.

--- Laboratorio4/Controllers/PlanetaController.cs
+++ Laboratorio4/Controllers/PlanetaController.cs
@@ -54,7 +54,7 @@ namespace Laboratorio4.Controllers
         [HttpGet]
         public ActionResult editarPlaneta(int? identificador)
         {
-
+            
             ActionResult vista;
             try
             {
---
+++ Laboratorio4/Models/FuncionarioModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Planetario.Models
+{
+    public class FuncionarioModel
+    {
+        public long NumeroIdentificacion { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Correo { get; set; }
+        public string Pais { get; set; }
+        public string Telefono { get; set; }
+        public string Descripcion { get; set; }
+        public string TituloAcademico { get; set; }
+        public string Ocupacion { get; set; }
+        public string Genero { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public List<string> Idiomas { get; set; }
+
+        public FuncionarioModel()
+        {
+
+        }
+    }
+}
